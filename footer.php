ed3136c8a4798be132b7c46bda34c4112e31083d
omarashzeinhom <omar.ashraf.zeinhom@Hotmail.com>
2024-02-15 13:32:25 +0200

    Updated index.php and Added home.php for overriding main page

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer>
+    <p>
+        <span class="copyright">
+            Copyright © <?php echo date('Y'); ?> Omar Ashraf Zeinhom - ANDGOEDU. All rights reserved.
+        </span>
+    </p>
+
+</footer>
--- index.php
+++ index.php
@@ -1,35 +1,17 @@
 <?php
-    /*
-Template Name: Home
-*/; ?>
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<?php
-wp_head();
-include('header.php');; ?>
+/*
+Template Name: Index
+*/
+?>
 
-<body>
-    <h1>Welcome to your new Professional Custom WordPress theme</h1>
-    <hr />
-    <h2>Development environment in </h2>
-    <ol>
-        <li> gulp
-        </li>
-        <li>livereload</li>
-        <li> webpack
-        </li>
-        <li> node
-        </li>
-        <li> xampp
-        </li>
-        <li> and wordpress
-        </li>
-    </ol>
-    <hr />
-    <p>Follow the setup instructions in the <code>README.md</code> or <code>TUTORIAL.md</code></p>
-    <main>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias, fugiat architecto doloribus cum ex ipsam voluptates commodi possimus eveniet facere sit corporis atque perferendis! Facere pariatur officia odio quidem corrupti!</p>
-    </main>
-</body>
+<?php get_header(); ?>
 
-</html>
+<div>
+    <?php the_content(); ?>
+</div>
+
+
+
+
+
+<?php get_footer(); ?>
