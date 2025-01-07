b7244576357356ae6d5ca1e7ecc5289276090110
omarashzeinhom <omar.ashraf.zeinhom@hotmail.com>
2025-01-07 13:23:07 +0200

    Updated webpack.config.js with modern-compiler to avoid any future issues refer to https://sass-lang.com/documentation/breaking-changes/legacy-js-api/

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,33 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/js/main.js',
+  entry: './src/js/main.js', // Entry point for JavaScript
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js', // Output file for JavaScript
+    path: path.resolve(__dirname, 'dist'), // Output directory
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/, // This rule applies to .scss files
+        use: [
+          'style-loader',   // Inject CSS into the DOM
+          'css-loader',     // Converts CSS into CommonJS
+          {
+            loader: 'sass-loader', // Compiles Sass to CSS
+            options: {
+              api: 'modern-compiler', // This ensures the modern Sass API is used
+              sassOptions: {
+                // You can specify additional Sass options here (optional)
+                // For example: outputStyle: 'compressed'
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  stats: {
+    loggingDebug: ['sass-loader'], // Enable debug logging for sass-loader
   },
 };
